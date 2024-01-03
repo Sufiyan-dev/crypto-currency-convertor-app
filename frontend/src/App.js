2ede7a5eed6dc6e7319252cda5d8497be33b6f42
Sufiyan <sufiyanmemon687@gmail.com>
2024-01-03 15:14:29 -0500

    added footer and fixed main css height

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
+import Footer from './Components/Footer';
 import PriceFetcher from './Components/PriceFetcher';
 
 function App() {
   return (
     <div className="App">
       <h1 className='h1-wrap'>Crypto Currency Converter</h1>
-      <div>
         <PriceFetcher/>
-      </div>
+        <Footer/>
     </div>
   );
 }
---
+++ frontend/src/Components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer-main'>
+        <p>Made by Sufiyan</p>
+        <p><a href="https://github.com/Sufiyan-dev/crypto-currency-convertor-app" target="_blank">GitHub Repository</a></p>
+    </div>
+  )
+}
+
+export default Footer
